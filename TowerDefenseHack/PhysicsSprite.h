df664572db9a8e1ba4d6ef293ca7eff9f985d274
Andrew Shaw <andrew.t.shaw@gmail.com>
2012-08-04 21:10:35 -0700

    Initial Commit

---
+++ TowerDefenseHack/PhysicsSprite.h
@@ -0,0 +1,19 @@
+//
+//  PhysicsSprite.h
+//  TowerDefenseHack
+//
+//  Created by Andrew Shaw on 8/4/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import "cocos2d.h"
+#import "chipmunk.h"
+
+@interface PhysicsSprite : CCSprite
+{
+	cpBody *body_;	// strong ref
+}
+
+-(void) setPhysicsBody:(cpBody*)body;
+
+@end
